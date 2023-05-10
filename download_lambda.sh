f00db82b33024debc935c4cd2b47b413d5b40f6d
dev-ai42 <124073578+dev-ai42@users.noreply.github.com>
2023-05-10 10:06:58 -0400

    Update lambda download script

--- download_lambda.sh
+++ download_lambda.sh
@@ -6,11 +6,11 @@ aws s3api get-object --bucket aws-well-architected-labs-ohio --key Reliability/R
 aws s3api get-object --bucket aws-well-architected-labs-ohio --key Reliability/MultiAZDBLambda.zip MultiAZDBLambda.zip
 aws s3api get-object --bucket aws-well-architected-labs-ohio --key Reliability/WebAppLambda.zip WebAppLambda.zip
 
-unzip VPCLambda.zip -d VPCLambda
-unzip WaitForStack.zip -d WaitForStack
-unzip RDSLambda.zip -d RDSLambda
-unzip MultiAZDBLambda.zip -d MultiAZDBLambda
-unzip WebAppLambda.zip -d WebAppLambda
+unzip -q VPCLambda.zip -d VPCLambda
+unzip -q WaitForStack.zip -d WaitForStack
+unzip -q RDSLambda.zip -d RDSLambda
+unzip -q MultiAZDBLambda.zip -d MultiAZDBLambda
+unzip -q WebAppLambda.zip -d WebAppLambda
 
 rm *Lambda.zip
 rm *Stack.zip
